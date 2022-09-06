592c5bf178a3205720fd6c03eab4a52e7ebda664
tjs78 <tjs78@cs-ssh.argo.uwf.edu>
2022-09-06 14:50:25 -0500

    adding params class

---
+++ Param.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+#include "Param.hpp"
+
+using namespace std;
+
+Params :: printParams() {
+
+	cout << "InputRedirect: [" << (inputRedirect != NULL) ? inputRedirect : "NULL" << "]" << endl << "OutputRedirect: [" << (outputRedirect != NULL) ? outputRedirect : "NULL" << "]" << endl << "Background: [" << background << "]" << endl << "ArgumentCount: [" << argumentCount << "]" << endl;
+
+	for(int i = 0; i < argumentCount; i++) {
+
+		cout << "ArgumentVector[" << i << "]: [" << argumentVector[i] << "]" << endl;
+
+	}
+
+}
+
---
+++ Param.hpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+#define MAXARGS 32
+
+class Params {
+
+	private:
+
+		char *inputRedirect;
+		char *outputRedirect;
+		int background;
+		int argumentCount;
+		char *argumentVector[MAXARGS];
+
+	public:
+
+		void printParams();
+
+};
+
